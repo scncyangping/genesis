3eabce84e982c6a3a4238b44c418f5ae783754ee
Yapi <hero.yp@qq.com>
2023-02-15 10:27:00 +0800

    feat(util): group any

--- pkg/util/convert.go
+++ pkg/util/convert.go
@@ -143,3 +143,34 @@ func UnCamelize(name string) string {
 	}
 	return buffer.String()
 }
+
+
+
+func ArrayInGroupsOf[T any](arr []T, num int) [][]T {
+	max := int(len(arr))
+	//判断数组大小是否小于等于指定分割大小的值，是则把原数组放入二维数组返回
+	if max <= num {
+		return [][]T{arr}
+	}
+	//获取应该数组分割为多少份
+	var quantity int
+	if max%num == 0 {
+		quantity = max / num
+	} else {
+		quantity = (max / num) + 1
+	}
+	//声明分割好的二维数组
+	var segments = make([][]T, 0)
+	//声明分割数组的截止下标
+	var start, end, i int
+	for i = 1; i <= quantity; i++ {
+		end = i * num
+		if i != quantity {
+			segments = append(segments, arr[start:end])
+		} else {
+			segments = append(segments, arr[start:])
+		}
+		start = i * num
+	}
+	return segments
+}
