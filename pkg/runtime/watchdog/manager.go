7f4edd2f65599e84fa6076045e0ce08f026c6012
Yapi <hero.yp@qq.com>
2023-03-13 16:51:41 +0800

    feat(watchdog): watchdog manager

---
+++ pkg/runtime/watchdog/manager.go
@@ -0,0 +1,59 @@
+package watchdog
+
+import (
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+type Manager struct {
+	watchDogs []Watchdog
+	logger    *zap.SugaredLogger
+}
+
+func NewWatchDogManager(lo *zap.SugaredLogger) *Manager {
+	return &Manager{
+		logger: lo,
+	}
+}
+
+func (w *Manager) WithWatchDog(wcs Watchdog) *Manager {
+	w.watchDogs = append(w.watchDogs, wcs)
+	return w
+}
+
+func (w *Manager) Start(stop <-chan struct{}) error {
+	w.logger.Infof("watchdog start")
+	errCh := make(chan error, 1)
+	defer close(errCh)
+
+	for _, wd := range w.watchDogs {
+		go func(w Watchdog, errCh chan<- error) {
+			// recover from a panic
+			defer func() {
+				if e := recover(); e != nil {
+					if err, ok := e.(error); ok {
+						errCh <- err
+					} else {
+						errCh <- errors.Errorf("%v", e)
+					}
+				}
+			}()
+			w.Start(stop)
+		}(wd, errCh)
+	}
+
+	select {
+	case <-stop:
+		w.logger.Info("watchdog done")
+		return nil
+	case err := <-errCh:
+		if err != nil {
+			w.logger.Error(errors.Wrap(err, "watchdog terminated with an error"))
+		}
+	}
+	return nil
+}
+
+func (w *Manager) NeedLeaderElection() bool {
+	return false
+}
